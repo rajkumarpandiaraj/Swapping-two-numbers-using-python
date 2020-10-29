1db310911eaa5086187b107f86fe9af0936710bd
rajkumar <rajkumarpandiyaraj@gmail.com>
2020-10-28 21:30:37 -0700

    swapping two numbers with/without third var

---
+++ withThirdvar.py
@@ -0,0 +1,8 @@
+num1 = int(input('Enter the first Number : '))
+num2 = int(input('Enter the second Number : '))
+temp = num1
+num1 = num2
+num2 = temp
+
+print('Swapped first Number : ', num1)
+print('Swapped second Number : ', num2)
---
+++ withoutThirdVar.py
@@ -0,0 +1,9 @@
+num1 = int(input('Enter the first Number : '))
+num2 = int(input('Enter the second Number : '))
+num1 = num1 + num2
+num2 = num1 - num2
+num1 = num1 - num2
+
+
+print('Swapped first Number : ', num1)
+print('Swapped second Number : ', num2)
